cee19a9d8533d58bbae2f265ed74c88522670d22
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-31 16:53:41 +0530

    MODIFIED: implemented class and retrying case of invalid input

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,18 +2,31 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public static void main(String args[]) {
-	System.out.println("Welcome to User Registration Problem!");
 	
-	Scanner str = new Scanner(System.in);
-	System.out.println("Enter your first name in ('S'anketh) format");
-	String fName = str.nextLine();
-	Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",fName);
-	if(firstName == true) {
-		System.out.println("First Name: " + fName);
-	}else {
-		System.out.println("Error! Invalid input from user please enter your First-Name in valid format(ex-'S'anketh).");
+	static String fName;
+
+	//uc-1 taking input from user
+	static String firstName(){
+		Scanner str1 = new Scanner(System.in);
+		System.out.println("Enter your first name in ('S'anketh) format: ");
+		fName = str1.nextLine();
+		return fName;
 	}
 
+	public static void main(String args[]) {
+		//welcome note for user
+		System.out.println("Welcome to User Registration Problem!");
+
+		//regex check for first name
+		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
+		
+		//loop for incorrect entry
+		while(firstName == false) {
+			System.out.println("Error! Invalid input from user please enter your First-Name in valid format(ex-'S'anketh).");
+			firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",UserRegistration.firstName());
+		}
+		
+		//printing the user input
+		System.out.println("First Name: " + fName);
 	}
 }
